03f4df1a57b3b32d4bf392d34cf00c288f4e8417
Moussa SARR <einsarr@gmail.com>
2019-12-30 22:10:31 +0000

    Netoyage

--- public/template/assets/footer.php
+++ public/template/assets/footer.php
@@ -1,11 +1,11 @@
  <!-- Footer -->
  <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
-          </div>
-        </div>
-      </footer>
+    <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <span>Copyright &copy; 2019. Tous les droits réservés à diti4.</span>
+      </div>
+    </div>
+  </footer>
       <!-- End of Footer -->
 
     </div>
@@ -14,29 +14,6 @@
   </div>
   <!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
   <!-- Bootstrap core JavaScript-->
   <script src="public/template/vendor//jquery/jquery.min.js"></script>
   <script src="public/template/vendor//bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -47,12 +24,7 @@
   <!-- Custom scripts for all pages-->
   <script src="public/template/js/sb-admin-2.min.js"></script>
 
-  <!-- Page level plugins -->
-  <script src="public/template/vendor//chart.js/Chart.min.js"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="public/template/js/demo/chart-area-demo.js"></script>
-  <script src="public/template/js/demo/chart-pie-demo.js"></script>
+ 
 
 </body>
 
