16c12a2a24bafd9514e39cb267767b856d93f941
Moussa SARR <einsarr@gmail.com>
2019-12-30 16:51:41 +0000

    Ajout du création des fichiers du dossier assets

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Login V2</title>
+  <title>Géolocalisation</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
@@ -10,9 +10,9 @@
 <!--===============================================================================================-->
   <link rel="stylesheet" type="text/css" href="public/login/css/bootstrap.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="public/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+  <link rel="stylesheet" type="text/css" href="public/login/fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
   <link rel="stylesheet" type="text/css" href="public/login/animate/animate.css">
 <!--===============================================================================================-->  
@@ -24,8 +24,8 @@
 <!--===============================================================================================-->  
   <link rel="stylesheet" type="text/css" href="public/login/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="css/util.css">
-  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="public/login/css/util.css">
+  <link rel="stylesheet" type="text/css" href="public/login/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -35,14 +35,14 @@
       <div class="wrap-login100">
         <form class="login100-form validate-form">
           <span class="login100-form-title p-b-26">
-            Welcome
-          </span>
-          <span class="login100-form-title p-b-48">
-            <i class="zmdi zmdi-font"></i>
+            CONNEXION
           </span>
 
           <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
-            <input class="input100" type="text" name="email">
+          <span class="btn-show-pass">
+            <i class="zmdi zmdi-email"></i>
+            </span>  
+          <input class="input100" type="text" name="email">
             <span class="focus-input100" data-placeholder="Email"></span>
           </div>
 
@@ -58,19 +58,9 @@
             <div class="wrap-login100-form-btn">
               <div class="login100-form-bgbtn"></div>
               <button class="login100-form-btn">
-                Login
+                Se Connecter
               </button>
             </div>
-          </div>
-
-          <div class="text-center p-t-115">
-            <span class="txt1">
-              Don’t have an account?
-            </span>
-
-            <a class="txt2" href="#">
-              Sign Up
-            </a>
           </div>
         </form>
       </div>
